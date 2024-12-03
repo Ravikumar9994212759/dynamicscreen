ea5dbd3159e950024489a6d904e8d6710c28ac0e
Ravikumar S. <ATE222@corpagaram.com>
2024-12-03 18:05:12 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,20 +1,11 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   swcMinify: true,
-// }
-
-// module.exports = nextConfig
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
- // basePath: '/dynamic',
   swcMinify: true,
   trailingSlash: true, 
-  output: 'standalone',
-  distDir: 'build',    
+  // basePath: '/dynamic', // Uncomment if you're deploying to a sub-path
+  // output: 'standalone',  // Comment out if not needed
+  distDir: '.next',       // Keep the default build folder unless necessary to change
 };
 
 module.exports = nextConfig;
